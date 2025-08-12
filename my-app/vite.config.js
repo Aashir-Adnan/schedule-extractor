219454e2b0f1be5bcf53248df024e8fbfab58c8b
Aashir-Adnan <aashiradnan99@gmail.com>
2025-08-12 21:27:24 +0500

    User Tracking

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/schedule-extractor/',
   server: {
     allowedHosts: [
       'aashir-schedule-extractinator-3000.loca.lt' 
